096489d886f2b48c2f5d55ca4255a9f06d1b1140
Arthur Schreiber <arthurschreiber@github.com>
2025-07-31 22:10:11 +0000

    Fix nil pointer exception.
    Signed-off-by: Arthur Schreiber <arthurschreiber@github.com>

--- go/vt/dbconnpool/connection_pool.go
+++ go/vt/dbconnpool/connection_pool.go
@@ -87,7 +87,9 @@ func (cp *ConnectionPool) Open(info dbconfigs.Connector) {
 	}
 
 	cp.ConnPool.Open(connect, refresh)
-	cp.statsExporter.SetPool(cp.ConnPool)
+	if cp.statsExporter != nil {
+		cp.statsExporter.SetPool(cp.ConnPool)
+	}
 }
 
 func (cp *ConnectionPool) Close() {
